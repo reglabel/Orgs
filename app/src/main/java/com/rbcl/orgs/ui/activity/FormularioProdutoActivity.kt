b54e49503f73d3e567a57077502c272f0fcbe776
Regla Bel <regla.belcl@gmail.com>
2023-07-18 10:42:01 -0300

    finalizando aula 3

--- app/src/main/java/com/rbcl/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/rbcl/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -2,6 +2,8 @@ package com.rbcl.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.rbcl.orgs.R
 import com.rbcl.orgs.dao.ProdutosDao
 import com.rbcl.orgs.databinding.ActivityFormularioProdutoBinding
 import com.rbcl.orgs.model.Produto
@@ -16,6 +18,17 @@ class FormularioProdutoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         configuraBotaoSalvar()
+        configuraDialogImagem()
+    }
+
+    private fun configuraDialogImagem() {
+        binding.activityFormularioProdutoImagem.setOnClickListener {
+            AlertDialog.Builder(this@FormularioProdutoActivity)
+                .setPositiveButton("Positivo") { _, _ -> }
+                .setNegativeButton("Negativo") { _, _ -> }
+                .setView(R.layout.formulario_imagem)
+                .show()
+        }
     }
 
     private fun configuraBotaoSalvar() {
@@ -30,13 +43,13 @@ class FormularioProdutoActivity : AppCompatActivity() {
     }
 
     private fun criaProduto(): Produto {
-        val campoNome = binding.activityFormularioProdutoNome
+        val campoNome = binding.activityFormularioProdutoNomeInputEdittext
         val nome = campoNome.text.toString()
 
-        val campoDescricao = binding.activityFormularioProdutoDescricao
+        val campoDescricao = binding.activityFormularioProdutoDescricaoInputEdittext
         val descricao = campoDescricao.text.toString()
 
-        val campoValor = binding.activityFormularioProdutoValor
+        val campoValor = binding.activityFormularioProdutoValorInputEdittext
         val valor = if (campoValor.text.toString().isBlank()) {
             BigDecimal.ZERO
         } else BigDecimal(campoValor.text.toString())
